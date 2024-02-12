8bfa4805ca32656160cb42aa018e75626a352a84
Максим Прядкин <120360513+Lahma-tut@users.noreply.github.com>
2024-02-12 12:28:08 +0300

    first commit

--- components/header/logo/Logo.jsx
+++ components/header/logo/Logo.jsx
@@ -1,11 +1,14 @@
 import s from "./logo.module.css";
 import Image from "next/image";
 import logo from "/public/logo.png";
+import Link from "next/link";
 
 export default function Logo() {
     return (
         <div className={s.logo}>
-            <Image src={ logo } alt="logo" width={266}/>
+            <Link href="/">
+                <Image src={ logo } alt="logo" width={266}/>
+            </Link>
         </div>
     );
 }
